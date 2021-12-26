13d32f662257c1c9e4c565ec2224004506b55206
kevink <kevinkaryadi@gmail.com>
2021-12-25 23:16:07 -0800

    first commit

---
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -0,0 +1,32 @@
+import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
+
+const BlogLayout = ({ children }) => {
+  const { logIn, logOut, isAuthenticated, userMetadata } = useAuth()
+  console.log('userMetadata', userMetadata)
+  return (
+    <>
+      <header>
+        <h1>
+          <Link to={routes.home()}>Redwood Blog</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            <li>
+              <button onClick={isAuthenticated ? logOut : logIn}>
+                {isAuthenticated ? 'Log Out' : 'Log In'}
+              </button>
+            </li>
+            {isAuthenticated && <li>{userMetadata.email}</li>}
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default BlogLayout
