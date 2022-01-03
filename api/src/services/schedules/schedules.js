257c76e68fbff3d8462ccd38831ca417040c8787
kevink <kevinkaryadi@gmail.com>
2022-01-02 19:19:08 -0800

    create members for every new schedule

--- api/src/services/schedules/schedules.js
+++ api/src/services/schedules/schedules.js
@@ -16,10 +16,64 @@ export const activeSchedule = () => {
   })
 }
 
-export const createSchedule = ({ input }) => {
-  return db.schedule.create({
+export const createSchedule = async ({ input }) => {
+  let record = await db.schedule.create({
     data: input,
   })
+  console.log('record' , record.id)
+  // get all members
+  let members = await db.user.findMany({
+    where: { member: true },
+  })
+
+  // // create signups for each members
+  // await members.map(async (member) => {
+  //   console.log('member' , member.id)
+  //   // var input = {
+  //   //   scheduleId: parseInt(record.id),
+  //   //   userId: parseInt(member.id)
+  //   // }
+  //   let data = {}
+  //   const castData = Object.assign(data, {
+  //     scheduleId: parseInt(record.id),
+  //     userId: parseInt(member.id)
+  //   })
+  //   console.log('castData', castData)
+  //   let memberRec = db.signUp.create({
+  //     data: castData,
+  //   })
+  //   console.log('memberRec id', memberRec.id)
+  // })
+
+  // let data = {}
+  // const castData = Object.assign(data, {
+  //   scheduleId: parseInt(17),
+  //   userId: parseInt(7)
+  // })
+  // console.log('castData 2', castData)
+  // let memberRec = await db.signUp.create({
+  //   data: castData,
+  // })
+  // console.log('memberRec id 2', memberRec.id)
+
+  async function createThem (members) {
+    for (const member of members) {
+      // console.log('member' , member.id)
+      let data = {}
+      const castData = Object.assign(data, {
+        scheduleId: parseInt(record.id),
+        userId: parseInt(member.id)
+      })
+      // console.log('castData', castData)
+      let memberRec = await db.signUp.create({
+        data: castData,
+      })
+      // console.log('memberRec id', memberRec.id)
+      }
+  }
+  createThem(members)
+
+  return record
 }
 
 export const updateSchedule = ({ id, input }) => {
